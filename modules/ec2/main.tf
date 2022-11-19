98790dfda96863ee8f418a2007183007aeb4f725
Pramodr677 <96058373+Pramodr677@users.noreply.github.com>
2022-11-19 15:19:09 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -23,9 +23,25 @@ resource "aws_instance" "nginx_client" {
   tags                        = var.tags_ec2_pub2
  
 }
+######################
+
+
+resource "aws_instance" "nginx_client2" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  availability_zone           = var.AZ2
+  subnet_id                   = var.subnet_id_public_1
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [var.security_id_nginxclient]
+  #count                      = var.public_node_count
+  tags                        = var.tags_ec2_pub3
+ 
+}
 
 
 
+#######
 
 
 resource "aws_instance" "nginx_node" {
