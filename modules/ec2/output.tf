32f376a3060027d588092e51fb900f54e3b22f91
Pramodr677 <96058373+Pramodr677@users.noreply.github.com>
2022-11-19 15:16:09 +0530

    Update output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -11,6 +11,10 @@ output "Intance_public_ip2" {
   value = aws_instance.nginx_client.public_ip
 }
 
+output "Intance_public_ip3" {
+  value = aws_instance.nginx_client2.public_ip
+}
+
 output "Instance_ips" {
   value = "${formatlist("%v", aws_instance.nginx_node.*.private_ip)}"
 }
